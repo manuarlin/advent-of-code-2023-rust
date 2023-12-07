8a9fce0492c769b0c34ee26fc42aa26e4f5cf11c
manuel.arlin <manuel.arlin@manomano.com>
2023-12-07 16:06:15 +0100

    day 7 part 2

--- src/day_7/day_7.rs
+++ src/day_7/day_7.rs
@@ -2,6 +2,9 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::hash::Hash;
 
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
 use crate::day_7::day_7::Card::{A, J, K, Number2, Number3, Number4, Number5, Number6, Number7, Number8, Number9, Q, T};
 use crate::utils::read_file;
 
@@ -39,6 +42,7 @@ pub fn day_7() {
 struct Hand {
     cards: [Card; 5],
     bid: i64,
+    hand_type: HandType,
 }
 
 impl PartialEq<Self> for Hand {
@@ -49,17 +53,17 @@ impl PartialEq<Self> for Hand {
 
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if self.hand_type() > other.hand_type() {
+        if self.hand_type > other.hand_type {
             return Some(Ordering::Greater);
         }
-        if self.hand_type() < other.hand_type() {
+        if self.hand_type < other.hand_type {
             return Some(Ordering::Less);
         }
-
         for (index, card) in self.cards.iter().enumerate() {
-            if card > &other.cards[index] {
+            let other_card = &other.cards[index];
+            if card > other_card {
                 return Some(Ordering::Greater);
-            } else if card < &other.cards[index] {
+            } else if card < other_card {
                 return Some(Ordering::Less);
             }
         }
@@ -69,44 +73,45 @@ impl PartialOrd for Hand {
 
 impl Hand {
     pub fn new(cards: [Card; 5], bid: i64) -> Self {
-        Self { cards, bid }
+        let hand_type: HandType = Hand::compute_hand_type_with_joker(&cards);
+        Self { cards, bid, hand_type }
     }
 
-    fn hand_type(&self) -> HandType {
+    fn compute_hand_type_without_joker(cards: &[Card; 5]) -> HandType {
         let mut map_of_equals: HashMap<&Card, i8> = HashMap::new();
 
-        for card in &self.cards {
+        for card in cards {
             if !map_of_equals.contains_key(card) {
                 map_of_equals.insert(card, 1);
             } else {
-                let current_value = map_of_equals.get(card).unwrap();
-                map_of_equals.insert(card, *current_value + 1);
+                let current_value = *map_of_equals.get(card).unwrap();
+                map_of_equals.insert(card, current_value + 1);
             }
         }
 
-        let mut number_of_equals: Vec<&i8> = map_of_equals.iter()
-            .map(|(_, value)| value)
+        let mut number_of_equals: Vec<i8> = map_of_equals.iter()
+            .map(|(_, value)| *value)
             .collect();
 
         number_of_equals.sort();
 
         let max_number_of_equals = *number_of_equals.last().unwrap();
 
-        if max_number_of_equals == &5 {
+        if max_number_of_equals == 5 {
             return HandType::FiveOfAKind;
         }
-        if max_number_of_equals == &4 {
+        if max_number_of_equals == 4 {
             return HandType::FourOfAKind;
         }
-        if max_number_of_equals == &3 {
-            return if *number_of_equals.get(number_of_equals.len() - 2).unwrap() > &1 {
+        if max_number_of_equals == 3 {
+            return if *number_of_equals.get(number_of_equals.len() - 2).unwrap() > 1 {
                 HandType::FullHouse
             } else {
                 HandType::ThreeOfAKind
             };
         }
-        if max_number_of_equals == &2 {
-            return if *number_of_equals.get(number_of_equals.len() - 2).unwrap() > &1 {
+        if max_number_of_equals == 2 {
+            return if *number_of_equals.get(number_of_equals.len() - 2).unwrap() > 1 {
                 HandType::TwoPairs
             } else {
                 HandType::OnePair
@@ -114,34 +119,58 @@ impl Hand {
         }
         HandType::HighCard
     }
+
+    fn compute_hand_type_with_joker(cards: &[Card; 5]) -> HandType {
+        if !cards.contains(&J) {
+            return Hand::compute_hand_type_without_joker(cards);
+        } else {
+            let mut best_hand_type = Hand::compute_hand_type_without_joker(&cards);
+            for joker_replacement_try in Card::iter() {
+                let mut new_cards: [Card; 5] = [J, J, J, J, J];
+                for (i, card) in cards.iter().enumerate() {
+                    if card == &J {
+                        new_cards[i] = joker_replacement_try;
+                    } else {
+                        new_cards[i] = *card;
+                    }
+                }
+                let new_cards_hand_type = Hand::compute_hand_type_without_joker(&new_cards);
+
+                if new_cards_hand_type > best_hand_type {
+                    best_hand_type = new_cards_hand_type;
+                }
+            }
+            best_hand_type
+        }
+    }
 }
 
 #[derive(Debug, PartialOrd, PartialEq)]
 enum HandType {
-    HighCard,
-    OnePair,
-    TwoPairs,
-    ThreeOfAKind,
-    FullHouse,
-    FourOfAKind,
-    FiveOfAKind,
+    HighCard = 1,
+    OnePair = 2,
+    TwoPairs = 3,
+    ThreeOfAKind = 4,
+    FullHouse = 5,
+    FourOfAKind = 6,
+    FiveOfAKind = 7,
 }
 
-#[derive(Debug, PartialOrd, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialOrd, PartialEq, Eq, Hash, EnumIter, Clone, Copy)]
 enum Card {
-    Number2,
-    Number3,
-    Number4,
-    Number5,
-    Number6,
-    Number7,
-    Number8,
-    Number9,
-    T,
-    J,
-    Q,
-    K,
-    A,
+    J = 1,
+    Number2 = 2,
+    Number3 = 3,
+    Number4 = 4,
+    Number5 = 5,
+    Number6 = 6,
+    Number7 = 7,
+    Number8 = 8,
+    Number9 = 9,
+    T = 10,
+    Q = 11,
+    K = 12,
+    A = 13,
 }
 
 impl Card {
@@ -164,3 +193,5 @@ impl Card {
         }
     }
 }
+
+
